824addff676b5f5d30ad16a142cec4c4df09b295
jerry <denggerry@163.com>
2018-12-13 12:33:46 +0800

    with stateless ui component
    if just has one render and no logic,it shoule use stateless componet to imporve performance

--- src/TodoUi.js
+++ src/TodoUi.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { Input, Button, List } from 'antd';
 
-class TodoUi extends React.Component {
-  render(){
-    return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input
-            value={this.props.inputValue}
-            placeholder={'todo info'}
-            style={{width:'300px', marginRight: '10px'}}
-            onChange={this.props.handleInputChange}>
-          </Input>
-          <Button type={"primary"} onClick={this.props.handleBtnClick}>submit</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (<List.Item onClick={(index) => {this.props.handleItemClick(index)}}>{item}</List.Item>)}
-        />
+const TodoUi = (props) => {
+  return (
+    <div style={{marginTop: '10px', marginLeft: '10px'}}>
+      <div>
+        <Input
+          value={props.inputValue}
+          placeholder={'todo info'}
+          style={{width:'300px', marginRight: '10px'}}
+          onChange={props.handleInputChange}>
+        </Input>
+        <Button type={"primary"} onClick={props.handleBtnClick}>submit</Button>
       </div>
-    )
-  }
-}
-
+      <List
+        style={{marginTop: '10px', width: '300px'}}
+        bordered
+        dataSource={props.list}
+        renderItem={(item) => (<List.Item onClick={(index) => {props.handleItemClick(index)}}>{item}</List.Item>)}
+      />
+    </div>
+  )}
 export default TodoUi;
